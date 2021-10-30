eae5b384c22c26e6cfd2201471bcae675e9ec7d0
Carlos Toxtli <ctoxtli@gmail.com>
2021-10-29 23:53:04 -0400

    Updates

---
+++ component---src-pages-404-js-16908f48a39a563644d6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),s=n.n(a);e.default=function(){return s.a.createElement("div",null,s.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-16908f48a39a563644d6.js.map
