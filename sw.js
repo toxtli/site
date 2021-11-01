4eb2eb1eb97466a1e31a88e0aa8d79bd48057e2d
Carlos Toxtli <ctoxtli@gmail.com>
2021-10-31 21:17:33 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6cb6c2e6cb57da630419.js"
+    "url": "webpack-runtime-a2e6cab86103a4b6e6b9.js"
   },
   {
     "url": "framework-81e6052b3504df28bf0b.js"
   },
   {
-    "url": "app-c2c6041f288be9169f45.js"
+    "url": "app-dc2eb0097925d7485da3.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d3b6bb3f6da6b02a875907b2c7552b8e"
+    "revision": "81c05345ffa32fc27fbe8d3ca968cfea"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c25716df4fbb2532700a.js"
@@ -48,11 +48,11 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f810816fd4f4377e1c58b63df724ce9c"
+    "revision": "b8196b0d78ca1b2f07669d635fc2f9e5"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "bfa93d3d9af1dde9d5763e904b14d45f"
+    "revision": "a4b5738955c570b9ae0d0594e7a0b5a6"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -145,7 +145,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/resume-template/app-c2c6041f288be9169f45.js`))) {
+  if (!resources || !(await caches.match(`/resume-template/app-dc2eb0097925d7485da3.js`))) {
     return await fetch(event.request)
   }
 
